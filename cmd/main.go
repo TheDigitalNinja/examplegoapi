195ebba57390cffd11732e24c033657ced46b79c
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2018-07-10 20:35:40 -0600

    removed old homeHandler

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,17 @@ package main
 
 import (
 	"examplegoapi/web/app/healthcheck"
+	"examplegoapi/web/app/home"
 
-	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-// HomeHandler TODO
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("../web/template/index.html"))
-	tmpl.Execute(w, nil)
-}
-
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/", home.Handler)
 	router.HandleFunc("/Status", healthcheck.Handler)
 	log.Print("Staring server on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
